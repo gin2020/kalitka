257cab9fc787291828c836da1bc1c03cc4d61542
gin2020 <demohhh@gmail.com>
2026-06-27 22:08:21 +0000

    создана api v1

---
+++ backend/app/api/v1/health.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+router = APIRouter(tags=["Health"])
+
+
+@router.get("/health")
+async def health():
+    return {
+        "status": "ok",
+        "service": "Kalitka API",
+    }
