32e1707ab92801a42139a1e385ad95a9e3d68bc8
gin2020 <demohhh@gmail.com>
2026-07-03 22:51:22 +0000

    create page личный кабинет

---
+++ frontend/src/app/my-vpn/page.tsx
@@ -0,0 +1,15 @@
+export default function MyVpnPage() {
+  return (
+    <main
+      style={{
+        maxWidth: 600,
+        margin: "40px auto",
+        padding: 24,
+      }}
+    >
+      <h1>🟢 Моя VPN</h1>
+
+      <p>Загрузка информации...</p>
+    </main>
+  );
+}
